a4d0cf4b4b0f347c505ed4177ebee566e039cab8
bharath <bharathprasannan96.com>
2022-12-29 14:35:30 +0530

    exercises component created

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,11 @@
 export const exerciseOptions =  {
-  method: 'GET',
-  headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': '8ac7723d8fmsh94da9e10d1afeacp1e18c6jsna04582e87ac8',
-      //'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-  }
-
+ 
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '38a288709cmshd45fb335362a8b0p153ae1jsn5063c3b58d3f',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  
 };
  export const youtubeOptions = {
   method: 'GET',
@@ -27,8 +27,9 @@ export const exerciseOptions =  {
 
 
 
-export const fetchData = async (url, options)=> {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    return data;
-}
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
